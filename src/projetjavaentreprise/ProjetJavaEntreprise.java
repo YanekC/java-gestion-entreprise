cbe0d2cb9ae6030ce828b67c0d8c6ef03316afd2
Alexandre Pausé <pausealexandre@gmail.com>
2018-01-21 22:21:13 +0100

    Branche

--- src/projetjavaentreprise/ProjetJavaEntreprise.java
+++ src/projetjavaentreprise/ProjetJavaEntreprise.java
@@ -16,7 +16,7 @@ public class ProjetJavaEntreprise {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Branche MASTER");
+        System.out.println("Branche DEVELOPPEMENT");
     }
     
 }
