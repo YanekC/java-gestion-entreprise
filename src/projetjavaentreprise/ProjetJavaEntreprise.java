4fb15d8b10966dfedeac7ed02899878c32bb07ac
Alexandre Pausé <pausealexandre@gmail.com>
2018-01-21 22:14:36 +0100

    RAZ

--- src/projetjavaentreprise/ProjetJavaEntreprise.java
+++ src/projetjavaentreprise/ProjetJavaEntreprise.java
@@ -3,19 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication3;
+
+package projetjavaentreprise;
 
 /**
  *
- * @author psl2094a
+ * @author user
  */
-public class JavaApplication3 {
+public class ProjetJavaEntreprise {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Branche MASTER");
     }
     
 }
