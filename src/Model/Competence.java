b7a6f03f0ebafee357e4eb42cc980007f28d952b
guilhem <guilhem@guilhemquintana>
2018-02-22 20:42:45 +0100

    ajout du model Competence

---
+++ src/Model/Competence.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author guilhem
+ */
+public class Competence {
+    private String idC;
+    private String libelleAng;
+    private String libelleFra;
+    
+    public Competence(String idC, String libelleAng, String libelleFra){
+        this.idC = idC;
+        this.libelleAng = libelleAng;
+        this.libelleFra = libelleFra;
+    }
+    
+    public String toString(){
+        return this.idC+" "+this.libelleAng+" "+this.libelleFra;
+    }
+    
+}
